fca082270c07a8fcf03aa1f013fd682bcbc11281
Semih Yumuşak <semihyumusak@yahoo.com>
2025-02-25 16:30:18 +0000

    Update main-publish-secure.py
    negotiation integration

--- main-publish-secure.py
+++ main-publish-secure.py
@@ -171,12 +171,79 @@ def ensure_string_values(data: Dict) -> Dict[str, str]:
     """Convert all values in a dictionary to strings."""
     return {k: str(v) if v is not None else "" for k, v in data.items()}
 
+def create_negotiation_offer(negotiation_provider_user_id, dataset, policy, distribution):
+    natural_language_document = ""
+
+    dataset["raw_object"] = dataset.copy()
+    if "upcast:price" in dataset:
+        dataset["price"] = dataset["upcast:price"]
+    if "@id" in dataset:
+        dataset["uri"] = dataset["@id"]
+    if "dct:title" in dataset:
+        dataset["title"] = dataset["dct:title"]
+    if "upcast:priceUnit" in dataset:
+        dataset["price_unit"] = dataset["upcast:priceUnit"]
+    if "odrl:hasPolicy" in dataset and "odrl_policy" in dataset and "uid" in dataset["odrl_policy"]:
+        dataset["policy_url"] = dataset["odrl_policy"]["uid"]
+    if "dct:description" in dataset:
+        dataset["description"] = dataset["dct:description"]
+    if "@type" in dataset:
+        dataset["type_of_data"] = dataset["@type"]
+    if "dct:publisher" in dataset and "@id" in dataset["dct:publisher"]:
+        dataset["publisher"] = dataset["dct:publisher"]["@id"]
+    if distribution is not {}:
+        dataset["distribution"] = ensure_string_values(distribution)
+
+    # Request body
+    payload_negotiation = {
+        "title": dataset['dct:title'],
+        "type": "offer",  # This will be overridden to "REQUEST" by the server
+        "consumer_id": negotiation_provider_user_id,
+        "producer_id": negotiation_provider_user_id,
+        "data_processing_workflow_object": {
+            # Include your data processing workflow details here
+            "workflow_steps": []
+        },
+        "natural_language_document": natural_language_document,
+        "resource_description_object": dataset,
+        "odrl_policy": policy
+    }
+
+    # Query parameters (the master password is passed as a query parameter)
+    params = {
+        "master_password_input": config.MASTER_PASSWORD  # Replace with actual master password
+    }
+
+    # Headers
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    # Make the POST request
+    response = requests.post(config.negotiation_url, params=params, headers=headers,
+                             data=json.dumps(payload_negotiation))
+
+    # Handle the response
+    if response.status_code == 200:
+        result = response.json()
+        offer_id = result['offer_id']
+
+        print("Offer created successfully!")
+        print(f"Offer ID: {result['offer_id']}")
+        print(f"Negotiation ID: {result['negotiation_id']}")
+        return offer_id
+    else:
+        print(f"Error: {response.status_code}")
+        print(response.text)
+        return None
+
+
 @app.post("/catalog/create_dataset_with_custom_fields/", dependencies=[Depends(verify_api_token)])
 async def create_dataset_with_custom_fields(body: Dict[str, Any]):
     backend = Backend()
     try:
         marketplaces = []
-        natural_language_document = ""
+        negotiation_provider_user_id = None
         policy = {}
         dataset = {}
         distribution = {}
@@ -209,64 +276,10 @@ async def create_dataset_with_custom_fields(body: Dict[str, Any]):
                 except BaseException as b:
                     raise HTTPException(status_code=400, detail="UPCAST object could not be parsed")
 
-        dataset["raw_object"] = dataset.copy()
-        if "upcast:price" in dataset:
-            dataset["price"] = dataset["upcast:price"]
-        if "@id" in dataset:
-            dataset["uri"] = dataset["@id"]
-        if "dct:title" in dataset:
-            dataset["title"] = dataset["dct:title"]
-        if "upcast:priceUnit" in dataset:
-            dataset["price_unit"] = dataset["upcast:priceUnit"]
-        if "odrl:hasPolicy" in dataset and "odrl_policy" in dataset and "uid" in dataset["odrl_policy"]:
-            dataset["policy_url"] = dataset["odrl_policy"]["uid"]
-        if "dct:description" in dataset:
-            dataset["description"] = dataset["dct:description"]
-        if "@type" in dataset:
-            dataset["type_of_data"] = dataset["@type"]
-        if "dct:publisher" in dataset and "@id" in dataset["dct:publisher"]:
-            dataset["publisher"] = dataset["dct:publisher"]["@id"]
-        if distribution is not {}:
-            dataset["distribution"] =ensure_string_values(distribution)
-
-        # Request body
-        payload_negotiation = {
-            "title": dataset['dct:title'],
-            "type": "offer",  # This will be overridden to "REQUEST" by the server
-            "consumer_id": negotiation_provider_user_id,
-            "producer_id": negotiation_provider_user_id,
-            "data_processing_workflow_object": {
-                # Include your data processing workflow details here
-                "workflow_steps": []
-            },
-            "natural_language_document": natural_language_document,
-            "resource_description_object": dataset,
-            "odrl_policy": policy
-        }
-
-        # Query parameters (the master password is passed as a query parameter)
-        params = {
-            "master_password_input": config.MASTER_PASSWORD  # Replace with actual master password
-        }
-
-        # Headers
-        headers = {
-            "Content-Type": "application/json"
-        }
-
-        # Make the POST request
-        response = requests.post(config.negotiation_url, params=params, headers=headers, data=json.dumps(payload_negotiation))
-
-        # Handle the response
-        if response.status_code == 200:
-            result = response.json()
-            body['extras'].append({"key": "negotiation_offer_id", "value": result["offer_id"]})
-            print("Offer created successfully!")
-            print(f"Offer ID: {result['offer_id']}")
-            print(f"Negotiation ID: {result['negotiation_id']}")
-        else:
-            print(f"Error: {response.status_code}")
-            print(response.text)
+        if negotiation_provider_user_id is not None:
+            offer_id = create_negotiation_offer(negotiation_provider_user_id, dataset, policy, distribution)
+            if offer_id is not None:
+                body['extras'].append({"key": "negotiation_offer_id", "value": offer_id})
 
         package_response = backend.create_backend_package_custom(body)
 
@@ -275,9 +288,9 @@ async def create_dataset_with_custom_fields(body: Dict[str, Any]):
                 for marketplace in marketplaces:
                     if marketplace == 'nokia' and 'created successfully' in package_response:
                         publish_nokia(upcast_object)
-                kafka_res = push_kafka_message("publishing-plugin", "publish", marketplace, body)
-                if kafka_res != True:
-                    message = str(kafka_res)
+                    kafka_res = push_kafka_message("publishing-plugin", "publish", marketplace, body)
+                    if kafka_res != True:
+                        message = str(kafka_res)
         except BaseException as b:
             message = " publish failed."
             pass
